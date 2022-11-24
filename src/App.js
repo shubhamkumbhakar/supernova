734063e131263ae987e23e5c56e56620b5b65f27
Shubham Kumbhakar <shubhamkumbhakar27@gmail.com>
2022-11-25 03:01:57 +0530

    Quiz shuffle options

--- src/App.js
+++ src/App.js
@@ -7,43 +7,113 @@ import Start from './Components/Start'
 const questions = [
     {
       id: 1,
-      leftImages: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToZDx4HZmsT4C7iB5zKM3KVE0u8MTHuOJWdj_gvBLn&s",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://thumbs.dreamstime.com/b/big-lord-shiva-statue-bangalore-18811578.jpg"
+      leftColumn: [
+        {
+          optionIndex: 0,
+          url: "https://theosophical.files.wordpress.com/2011/06/zero2.jpg"
+        },
+        {
+          optionIndex: 1,
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS5CNumDtlLMWO1jGCPmV1yJ-xjE2e0842QX_OKV45JteaROfOubbRgl3qi5EyAEpmwhc&usqp=CAU"
+        },
+        {
+          optionIndex: 2,
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcBGsvgDYtlSc6m_NiFj6z1jZmcF3YA-Nf_rh5a3fI6OJWNgQAANITICydncfeTFGQPek&usqp=CAU",
+        },
+        {
+          optionIndex: 3,
+          url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ94l1mPbLGLAuJVdI_GDD6W4qvL7KpqI0IdU4ezWZuDC3ghTq9V1HCbHujXMmJk7F0x0w&usqp=CAU",
+        }
       ],
-      rightImages: [
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg"
+      rightColumn: [
+        {
+          optionIndex: 0,
+          url: "https://toytheater.com/wp-content/uploads/alphabet_tiles_2.gif",
+        },
+        {
+          optionIndex: 1,
+          url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/LetterB.svg/640px-LetterB.svg.png",
+        },
+        {
+          optionIndex: 2,
+          url:"https://cdn3.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_C_red-512.png",
+        },
+        {
+          optionIndex: 3,
+          url: "https://cdn3.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_D_blue-512.png"
+        }
       ],
-      correctMatch: [1,2,0]
+      correctMatch: [3,2,1,0]
     },
     {
-      id: 2,
-      leftImages: [
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg"
+      id: 1,
+      leftColumn: [
+        {
+          optionIndex: 0,
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS5CNumDtlLMWO1jGCPmV1yJ-xjE2e0842QX_OKV45JteaROfOubbRgl3qi5EyAEpmwhc&usqp=CAU"
+        },
+        {
+          optionIndex: 1,
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcBGsvgDYtlSc6m_NiFj6z1jZmcF3YA-Nf_rh5a3fI6OJWNgQAANITICydncfeTFGQPek&usqp=CAU",
+        },
+        {
+          optionIndex: 2,
+          url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ94l1mPbLGLAuJVdI_GDD6W4qvL7KpqI0IdU4ezWZuDC3ghTq9V1HCbHujXMmJk7F0x0w&usqp=CAU",
+        },
+        {
+          optionIndex: 3,
+          url: "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg"        
+        }
       ],
-      rightImages: [
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg"
+      rightColumn: [
+        {
+          optionIndex: 0,
+          url: "https://toytheater.com/wp-content/uploads/alphabet_tiles_2.gif",
+        },
+        {
+          optionIndex: 1,
+          url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/LetterB.svg/640px-LetterB.svg.png",
+        },
+        {
+          optionIndex: 2,
+          url:"https://cdn3.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_C_red-512.png",
+        },
+        {
+          optionIndex: 3,
+          url: "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg"
+        }
       ],
       correctMatch: [1,2,0]
     },
     {
       id: 1,
-      leftImages: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToZDx4HZmsT4C7iB5zKM3KVE0u8MTHuOJWdj_gvBLn&s",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://thumbs.dreamstime.com/b/big-lord-shiva-statue-bangalore-18811578.jpg"
+      leftColumn: [
+        {
+          optionIndex: 0,
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS5CNumDtlLMWO1jGCPmV1yJ-xjE2e0842QX_OKV45JteaROfOubbRgl3qi5EyAEpmwhc&usqp=CAU"
+        },
+        {
+          optionIndex: 1,
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcBGsvgDYtlSc6m_NiFj6z1jZmcF3YA-Nf_rh5a3fI6OJWNgQAANITICydncfeTFGQPek&usqp=CAU",
+        },
+        {
+          optionIndex: 2,
+          url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ94l1mPbLGLAuJVdI_GDD6W4qvL7KpqI0IdU4ezWZuDC3ghTq9V1HCbHujXMmJk7F0x0w&usqp=CAU",
+        }
       ],
-      rightImages: [
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg",
-        "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg",
-        "https://shubhamkumbhakar.in/static/media/pp.23c6fc659b84fe5423f2.jpeg"
+      rightColumn: [
+        {
+          optionIndex: 0,
+          url: "https://toytheater.com/wp-content/uploads/alphabet_tiles_2.gif",
+        },
+        {
+          optionIndex: 1,
+          url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/LetterB.svg/640px-LetterB.svg.png",
+        },
+        {
+          optionIndex: 2,
+          url:"https://cdn3.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_C_red-512.png",
+        }
       ],
       correctMatch: [1,2,0]
     }
--- src/Components/Quiz/index.jsx
+++ src/Components/Quiz/index.jsx
@@ -1,13 +1,28 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './style.css'
 import sound from '../../Assets/buttonclick.mp3'
 const Quiz = (props) => {
   const [audio] = useState(new Audio(sound));
   const [currQues, setCurrentQues] = useState(0);
-  const {questions} = props.states;
+  const [activeElemLeft, setActiveElemLeft] = useState(-1);
+  const [activeElemRight, setActiveElemRight] = useState(-1);
+  const {questions} = props.states
+  const [leftColumn, setLeftColumn] = useState([]);
+  const [rightColumn, setRightColumn] = useState([]);
   const {setCurrentScreen} = props.actions;
+  const [totalMatch, setTotalMatch] = useState(0);
+  const [matches, setMatches] = useState([]);
+  const [matchedLeft, setMatchedLeft] = useState(new Set());
+  const [matchedRight, setMatchedRight] = useState(new Set());
+  const reset = () => {
+    setMatches([]);
+    setLeftColumn(questions[currQues].leftColumn);
+    setRightColumn(questions[currQues].rightColumn);
+    setTotalMatch(0);
+  }
   const nextQues = () =>{
     audio.play();
+    reset()
     const n = questions.length;
     if(currQues<n-1){
         setCurrentQues(currQues+1);
@@ -15,23 +30,102 @@ const Quiz = (props) => {
         setCurrentScreen('score');
     }
   }
+
+  const unMatch = () => {
+    let i = activeElemLeft>=0?activeElemLeft:activeElemRight;
+    while(i<totalMatch-1){
+        const tempLeft = leftColumn[i];
+        leftColumn[i] = leftColumn[i+1];
+        leftColumn[i+1] = tempLeft;
+
+        const tempRight = rightColumn[i];
+        rightColumn[i] = rightColumn[i+1];
+        rightColumn[i+1] = tempRight;
+        
+        i++;
+    }
+    setLeftColumn(leftColumn);
+    setRightColumn(rightColumn);
+    setTotalMatch(totalMatch-1);
+  }
+  const match = () => {
+    let i = activeElemLeft;
+    let j = activeElemRight;
+    
+    while(i>totalMatch){
+        const temp = leftColumn[i];
+        leftColumn[i] = leftColumn[i-1];
+        leftColumn[i-1] = temp;
+        i--;
+    }
+    setLeftColumn(leftColumn);
+    while(j>totalMatch){
+        const temp = rightColumn[j];
+        rightColumn[j] = rightColumn[j-1];
+        rightColumn[j-1] = temp;
+        j--;
+    }
+    setRightColumn(rightColumn)
+    setTotalMatch(totalMatch+1);
+    
+  }
+
+  useEffect(()=>{
+    setLeftColumn(questions[currQues].leftColumn);
+    setRightColumn(questions[currQues].rightColumn);
+  },[currQues])
+
+
+  useEffect(()=>{
+    if(activeElemLeft>=0 && activeElemLeft>=totalMatch && activeElemRight>=0 && activeElemRight>=totalMatch){
+        matches[leftColumn[activeElemLeft].optionIndex] = rightColumn[activeElemRight].optionIndex;
+        setMatches(matches);
+        console.log(matches);
+
+        match();
+        
+        setActiveElemLeft(-1);
+        setActiveElemRight(-1);
+    }else if((activeElemLeft>=0 && activeElemLeft<totalMatch) || (activeElemRight>=0 && activeElemRight<totalMatch)){
+        unMatch();
+        setActiveElemLeft(-1);
+        setActiveElemRight(-1);
+    }
+  },[activeElemLeft,activeElemRight])
   return (
         <>
-        <div className='table'>
-            <div className='left'>
+        <div> Match the following. </div>
+        <div className='quiz__table'>
+            <div className='quiz__left'>
                 {
-                    questions[currQues].leftImages.map((image)=>(
-                        <div className='imagebox'>
-                            <img src={image} alt=""/>
+                    leftColumn.map((image, i)=>(
+                        <div key={i} id={`imgLeft${i}`} className={activeElemLeft===i?"quiz__imagebox active-left":i<totalMatch?"quiz__imagebox matched-left":"quiz__imagebox"} onClick={()=>{
+                            console.log(activeElemLeft);
+                            if(activeElemLeft===i){
+                                setActiveElemLeft(-1);
+                            }else{
+                                setActiveElemLeft(i);
+                            }
+                        }}>
+                            <img src={image.url} style={{maxHeight:'15vh', maxWidth:'15vh'}}  alt=""/>
                         </div>
                     ))
                 }
             </div>
-            <div className='right'>
+            <div className='quiz__right'>
                 {
-                    questions[currQues].rightImages.map((image)=>(
-                        <div className='imagebox'>
-                            <img src={image} alt=""/>
+                    rightColumn.map((image, i)=>(
+                        <div key={i} id={`imgRight${i}`} className={activeElemRight===i?"quiz__imagebox active-right":i<totalMatch?"quiz__imagebox matched-right":"quiz__imagebox"} onClick={()=>{
+                            console.log(activeElemRight);
+                            if(activeElemRight===i){
+                                setActiveElemRight(-1);
+                            }else{
+                                setActiveElemRight(i);
+                            }
+                            
+                            
+                        }}>
+                            <img src={image.url} style={{maxHeight:'15vh', maxWidth:'15vh'}} alt=""/>
                         </div>
                     ))
                 }
